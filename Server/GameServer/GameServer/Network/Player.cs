7d45b48e34ddfaebf0b445ba1cfde25c0d899ab9
EomTaeWook <trim0917@gmail.com>
2025-05-24 02:52:17 +0900

    멀티 게임을 위한 게임서버 커밋

---
+++ Server/GameServer/GameServer/Network/Player.cs
@@ -0,0 +1,19 @@
+﻿using Dignus.Sockets.Interfaces;
+
+namespace BG.GameServer.Network
+{
+    internal class Player
+    {
+        private ISession _session;
+
+        public Player(ISession session)
+        {
+            _session = session;
+        }
+
+        public void Send(IPacket packet)
+        {
+            _session.TrySend(packet);
+        }
+    }
+}
---
+++ Server/GameServer/GameServer/Program.cs
@@ -0,0 +1,40 @@
+﻿using BG.GameServer.Internals;
+using Dignus.DependencyInjection;
+using Dignus.DependencyInjection.Extensions;
+using Dignus.Log;
+using System.Reflection;
+
+namespace BG.GameServer
+{
+    internal class Program
+    {
+        static void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            LogHelper.Error(e.ExceptionObject as Exception);
+            Environment.Exit(1);
+        }
+        static void Main(string[] args)
+        {
+            AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
+            LogBuilder.Configuration(LogConfigXmlReader.Load("DignusLog.config")).Build();
+
+            IServiceProvider serviceProvider = RegisterDependencies();
+
+
+            GameServerNode gameServer = serviceProvider.GetService<GameServerNode>();
+
+            gameServer.Start(40000);
+
+            Console.ReadKey();
+        }
+
+        public static IServiceProvider RegisterDependencies()
+        {
+            var serviceContainer = new ServiceContainer();
+
+            serviceContainer.RegisterDependencies(Assembly.GetExecutingAssembly());
+
+            return serviceContainer.Build();
+        }
+    }
+}
